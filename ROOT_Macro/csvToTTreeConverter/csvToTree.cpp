b8c0391d397ff15fba78a63f0b00591365c969c0
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-07-12 15:07:35 +0200

    New branch have been added to the TTree + parsing core has been updated
    Empty lines at the end of the RTI files are now parsed correctly

--- ROOT_Macro/csvToTTreeConverter/csvToTree.cpp
+++ ROOT_Macro/csvToTTreeConverter/csvToTree.cpp
@@ -7,9 +7,11 @@
 
 #include "TTree.h"
 #include "TFile.h"
+#include "TMath.h"
 
 #define words_in_line 54
 #define glat_word_counter 23
+#define thetas_word_counter 19
 
 std::string readStreamFromFile(std::string input_RTI_path)
 {
@@ -27,7 +29,12 @@ std::string readStreamFromFile(std::string input_RTI_path)
 void parse_stream(
     const std::string input_RTI_path,
     double &glat, 
-    double &glon, 
+    double &glon,
+    double &thetas,
+    double &phi,
+    int &events,
+    double &geo_lat,
+    double &geo_lon,
     TTree &RTI_tree)
 {
     std::istringstream input_stream(readStreamFromFile(input_RTI_path));
@@ -38,7 +45,7 @@ void parse_stream(
         std::istringstream rti_stream(readStreamFromFile(tmp_RTI_file));
         // Create line counter variable
         unsigned int line_counter = 0;
-
+        
         std::string tmp_str;
         std::string header;
         std::string::size_type sz;
@@ -46,37 +53,45 @@ void parse_stream(
         // parse stream
         //while (input_stream >> tmp_str)
         rti_stream >> header;
-        
-        while(!rti_stream.eof())
-        {
-            rti_stream >> tmp_str;
-            auto JMDCtime = stoul(tmp_str, &sz);
-            rti_stream >> tmp_str;
-            auto run = stoul(tmp_str, &sz);
-            if (!run)
-            {
-                ++line_counter;
-                continue;
-            }
+        int wcounter = 0;
 
-            // skipping first glat_word_counter -1 words and read the glat_word_counter-th word
-            for (auto wIdx=0; wIdx<(glat_word_counter-2); ++wIdx)
-                rti_stream >> tmp_str;
-            
-            glat = stod(tmp_str, &sz);
-            rti_stream >> tmp_str;
-            glon = stod(tmp_str, &sz);
-            
-            // Finish reading RTI line
-            for (auto wIdx=0; wIdx<(words_in_line-(glat_word_counter+1)); ++wIdx)
-                rti_stream >> tmp_str;
-            
-            ++line_counter;
-            
-            RTI_tree.Fill();
+        while(rti_stream >> tmp_str)
+        {
+            ++wcounter;
+
+            if (wcounter == 1)
+                auto JMDCtime = stoul(tmp_str, &sz);
+            else if (wcounter == 2)
+            {
+                auto run = stoul(tmp_str, &sz);
+                if (!run)
+                {
+                    ++line_counter;
+                    wcounter=0;
+                    continue;
+                }
+            }
+            else if (wcounter == thetas_word_counter)
+                thetas = stod(tmp_str, &sz);
+            else if (wcounter == (thetas_word_counter+1))
+                phi = stod(tmp_str, &sz);
+            else if (wcounter == glat_word_counter) 
+                glat = stod(tmp_str, &sz);
+            else if (wcounter == (glat_word_counter+1))
+                glon = stod(tmp_str, &sz);
+            else if (wcounter == (glat_word_counter+2))
+                events = stoi(tmp_str, &sz);
+            else if (wcounter == words_in_line)
+            {
+                wcounter=0;
+                ++line_counter;
+                geo_lat = (1-thetas)*TMath::RadToDeg();
+                geo_lon = phi*TMath::RadToDeg();
+                RTI_tree.Fill();
+            }
         }
         
-        std::cout << "\nHas been read " << line_counter << " lines from: " << tmp_RTI_file << std::endl;
+        std::cout << "\nHas been read " << line_counter << " lines from: " << tmp_RTI_file;
     }
 }
 
@@ -85,6 +100,11 @@ void csvToTree(std::string input_RTI_path, std::string outFile)
     // Create glon e glat variables
     double glat = -1;
     double glon = -1;
+    double thetas = -1;
+    double phi = -1;
+    int events = -1;
+    double geo_lat = -1;
+    double geo_lon = -1;
 
     // Create new TFile
     TFile RTI_output_file(outFile.c_str(),"RECREATE");
@@ -99,12 +119,22 @@ void csvToTree(std::string input_RTI_path, std::string outFile)
     // Branch the TTree
     RTI_tree.Branch("glat", &glat, "glat/D");
     RTI_tree.Branch("glon", &glon, "glon/D");
+    RTI_tree.Branch("thetas", &thetas, "thetas/D");
+    RTI_tree.Branch("phi", &phi, "phi/D");
+    RTI_tree.Branch("events", &events, "events/I");
+    RTI_tree.Branch("geo_lat", &geo_lat, "geo_lat/D");
+    RTI_tree.Branch("geo_lon", &geo_lon, "geo_lon/D");
 
     // Parse stream
     parse_stream(
         input_RTI_path,
         glat, 
-        glon, 
+        glon,
+        thetas,
+        phi,
+        events,
+        geo_lat,
+        geo_lon,
         RTI_tree);
     
     RTI_tree.Write();
